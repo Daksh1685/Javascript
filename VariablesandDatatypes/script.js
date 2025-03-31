eff7ea64e9d6f3cb186a37e4ab6e1621e1fbe036
Daksh1685 <dakshchaurasia18@gmail.com>
2025-03-31 15:15:04 +0530

    Initial commit

---
+++ VariablesandDatatypes/script.js
@@ -0,0 +1,46 @@
+console.log("Hey this is Daksh")
+
+// let a = 5;
+// var b = 6;
+// var c = "Daksh";
+// console.log(a+b+46,c)
+// console.log(typeof a, typeof b, typeof c);
+// javascript is case sensitive
+// var is a globally scoped whereas let and const are block scoped
+
+// const a1 = 6;
+// a1 = a1 + 6 Not allowed,const cannot be changed
+//  let a = 56;
+//  {
+//     let a = 98;
+//     console.log(a)
+//  }
+//  console.log(a)
+
+
+ var a = 5652
+ {
+    var a = 568;
+    console.log(a)
+ }
+ console.log(a)
+
+let x = "daksh Bhai";
+let y = 20;
+let z = 3.56;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+// object
+let o = {
+    "name": "Daksh",
+    "job code": "800"
+}
+console.log(o)
+o.salary = "500crores"
+console.log(o)
+o.salary = "5000crores"
+console.log(o)
