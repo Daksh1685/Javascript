1499408319032832ec984971d515dfbfa0f083e3
Daksh1685 <dakshchaurasia18@gmail.com>
2025-04-06 14:48:19 +0530

    Initial commit

---
+++ DOM/index.js
@@ -0,0 +1,16 @@
+console.log("Hello world")
+document.body.firstElementChild 
+document.body.firstElementChild.childNodes 
+document.body.firstElementChild.children
+document.body.lastElementChild
+document.body.lastElementChild.childNodes
+document.body.lastElementChild.children
+document.body.parentElement
+document.body.parentNode
+document.body.nextElementSibling
+document.body.nextSibling
+document.body.previousElementSibling
+document.body.previousSibling
+document.body.children
+document.body.children[1]
+document.body.children[1].rows
